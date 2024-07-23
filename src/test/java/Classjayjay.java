ded5a8caeb13436e45819bd22f61dc86d6f86de3
Okky-nababan <okkyalex36@gmail.com>
2024-07-23 14:15:53 +0800

    Add files via upload

---
+++ src/test/java/Classjayjay.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class Classjayjay {
+
+    @Test
+    public void testPenjumlahaan(){
+    }
+}
---
+++ src/test/java/Jayjaylesson.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Jayjaylesson {
+
+    @Test
+    public void testPenjumlahan(){
+        System.out.println("Dieksekusi dalam test");
+        Assertions.assertEquals(8,4+4);
+    }
+}
